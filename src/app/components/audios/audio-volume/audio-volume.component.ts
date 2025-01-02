78b2860fe9de708d9887cf7f80abc0cc182113d9
Ayoub CHAABAT <chaabat.02@gmail.com>
2025-01-02 10:00:15 +0100

    track managment

--- src/app/components/audios/audio-volume/audio-volume.component.ts
+++ src/app/components/audios/audio-volume/audio-volume.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-audio-volume',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './audio-volume.component.html',
-  styleUrl: './audio-volume.component.css'
+  styleUrl: './audio-volume.component.css',
 })
 export class AudioVolumeComponent {
+  @Input() volume: number = 1;
+  @Output() volumeChange = new EventEmitter<number>();
 
+  onVolumeInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.volumeChange.emit(Number(input.value));
+  }
 }
